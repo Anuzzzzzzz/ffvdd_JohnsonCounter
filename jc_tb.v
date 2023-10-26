8df2c88682335c715e94b62164a3b5dde9f5c74b
Anuzzzzzzz <148976244+Anuzzzzzzz@users.noreply.github.com>
2023-10-26 17:32:31 +0530

    Update jc_tb.v

--- jc_tb.v
+++ jc_tb.v
@@ -1,17 +1,14 @@
-module iiitb_jc_tb;
+module jc_tb;
 reg clk;
 reg rst;
 wire [7:0]q;
-iiitb_jc j1(rst,clk,q);
+jc j1(rst,clk,q);
 
 initial
 begin
 clk=0;
 rst = 0;
 
-//$monitor($time, ,”c=%b”,clk, , ,”r=%b”,rst, , ,”q=%b”,q);
-$dumpfile("iiitb_jc.vcd");
-$dumpvars(1);
 #6 rst =1;
 end
 
